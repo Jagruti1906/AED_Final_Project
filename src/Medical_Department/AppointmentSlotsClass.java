2fb3b8235d6c3928b91ef7ce84ca9de1fe513cd6
Jagruti1906 <jagruti1906@gmail.com>
2022-12-02 14:00:56 -0500

    Appointment Slots Class and Directory Created

---
+++ src/Medical_Department/AppointmentSlotsClass.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Medical_Department;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jagru
+ */
+public class AppointmentSlotsClass {
+    private Date date;
+    private String slotTime;
+    private String status;
+
+    public AppointmentSlotsClass(Date date, String slotTime, String status) {
+        this.date = date;
+        this.slotTime = slotTime;
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSlotTime() {
+        return slotTime;
+    }
+
+    public void setSlotTime(String slotTime) {
+        this.slotTime = slotTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
