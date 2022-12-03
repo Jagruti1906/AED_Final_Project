de5a6c3c2013b3b43d208421f25ea9f37c1603e9
sarvesh3737 <malpani.sa@northeastern.edu>
2022-12-03 18:19:31 -0500

    Register Resident functions added

--- src/Medical_Department/PatientDirectory.java
+++ src/Medical_Department/PatientDirectory.java
@@ -4,7 +4,16 @@
  */
 package Medical_Department;
 
+import aed_project.DatabaseConnectionClass;
+import UI.RegisterDoctor;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -20,6 +29,32 @@ public class PatientDirectory {
     
     public void addPatient(PatientClass pat){
         patientDir.add(pat);
+        Statement stmt;
+        try {
+            stmt = DatabaseConnectionClass.getInstance().getCon().createStatement();
+            String query1 = "INSERT INTO doctor" + " VALUES(?,?,?,?,?,?,?,?,?,?)";
+            java.sql.Date sqlDate = new java.sql.Date(pat.getDob().getTime());
+            PreparedStatement pst = DatabaseConnectionClass.getInstance().getCon().prepareStatement(query1);
+            pst.setInt(1, pat.getStateID());
+            pst.setString(2, pat.getName());
+            pst.setInt(3, pat.getAge());
+            pst.setInt(4, pat.getdoctorId());
+            pst.setString(5, pat.getGender());
+            pst.setString(6, pat.getEmail());
+            pst.setInt(7, pat.getPhoneNumber());
+            pst.setDate(8, sqlDate);
+            pst.setString(9, pat.getSpecialisation());
+            pst.setString(10, pat.getHospitalName());
+            int rs = pst.executeUpdate();
+            if(rs>0)
+            {
+                JOptionPane.showMessageDialog(null,"Inserted Successfully!");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RegisterDoctor.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null,"Cannot be Inserted");
+        }
+    
     }
 
     public ArrayList<PatientClass> getPatientDir() {
--- src/Resident/ResidentDirectory.java
+++ src/Resident/ResidentDirectory.java
@@ -3,21 +3,62 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Resident;
-
+import UI.RegisterDoctor;
+import aed_project.DatabaseConnectionClass;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author jagru
  */
 public class ResidentDirectory {
-    private ArrayList<ResidentClass> residentDir;
+    private static ArrayList<ResidentClass> residentDir = null;
+     private static ResidentDirectory mInstance;
 
-    public ResidentDirectory(ArrayList<ResidentClass> residentDir) {
-        this.residentDir = residentDir;
+    private ResidentDirectory() {
+        this.residentDir = new ArrayList();
     }
 
     public ArrayList<ResidentClass> getResidentDir() {
         return residentDir;
+    }
+    public void addResident(ResidentClass res){
+        residentDir.add(res);
+        Statement stmt;
+        try {
+            stmt = DatabaseConnectionClass.getInstance().getCon().createStatement();
+            String query1 = "INSERT INTO resident" + " VALUES(?,?,?,?,?,?,?,?,?)";
+            java.sql.Date sqlDate = new java.sql.Date(res.getDob().getTime());
+            PreparedStatement pst = DatabaseConnectionClass.getInstance().getCon().prepareStatement(query1);
+            pst.setInt(1, res.getStateID());
+            pst.setString(2, res.getName());
+            pst.setInt(3, res.getAge());
+            pst.setString(4, res.getGender());
+            pst.setString(5, res.getEmail());
+            pst.setInt(6, res.getPhoneNumber());
+            pst.setDate(7, sqlDate);
+            pst.setString(8, res.getAddress());
+            pst.setInt(9, res.getZip());
+            int rs = pst.executeUpdate();
+            if(rs>0)
+            {
+                JOptionPane.showMessageDialog(null,"Inserted Successfully!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Cannot be Inserted");
+        }
+    }
+        public static ResidentDirectory getInstance() {
+        if(mInstance == null)
+            mInstance = new ResidentDirectory();
+
+        return mInstance;
     }
 }
